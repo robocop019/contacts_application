ce45a0176bb1a98f58e1558d4b92a9a48e270083
robocop019 <chrisbowen@comcast.net>
2020-02-26 18:59:27 -0600

    added validations and happy sad paths

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,4 +1,9 @@
 class Contact < ApplicationRecord
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+
+  validates :email, uniqueness: true
+
   def full_name
     "#{first_name} #{middle_name} #{last_name}"
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,17 @@
 
-1000.times do 
-  first_name = Faker::Name.first_name
-  last_name = Faker::Name.last_name
+# 1000.times do 
+#   first_name = Faker::Name.first_name
+#   last_name = Faker::Name.last_name
 
-  contact = Contact.new(
-                        first_name: first_name,
-                        middle_name: Faker::Name.middle_name,
-                        last_name: last_name,
-                        email: Faker::Internet.free_email(name: "#{first_name}.#{last_name}"),
-                        phone_number: Faker::PhoneNumber.phone_number,
-                        bio: Faker::TvShows::Buffy.quote
-                        )
+#   contact = Contact.new(
+#                         first_name: first_name,
+#                         middle_name: Faker::Name.middle_name,
+#                         last_name: last_name,
+#                         email: Faker::Internet.free_email(name: "#{first_name}.#{last_name}"),
+#                         phone_number: Faker::PhoneNumber.phone_number,
+#                         bio: Faker::TvShows::Buffy.quote
+#                         )
 
-  contact.save
-end
+#   contact.save
+# end
 
